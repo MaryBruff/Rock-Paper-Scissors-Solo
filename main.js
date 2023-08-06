33ec14636c51e64f23c7fedda967c2b5ab635667
Mary Bruff <marybruff5@gmail.com>
2023-08-06 13:14:57 -0600

    add create player function and create game function as well as check win function

--- main.js
+++ main.js
@@ -1,59 +1,87 @@
-//QUERY SELECTORS:
+//Global Variables:
+var fighterClassicOptions = ['rock', 'paper', 'scissors']
+var fighterDifficultOptions = ['rock', 'paper', 'scissors', 'the architect', 'sentinel']
+var changeGame
+var game = createGame()
 
+
+//QUERY SELECTORS:///
 ///game types:///
 var classicContainer = document.querySelector('.classic-container');
 var difficultContainer = document.querySelector('.difficult-container');
+
 ///choose your game and fighter text:///
-var pageLoadMessage = document.querySelector(".page-load-message");
-var pickYourGame = document.querySelector(".pick-your-game");
-///icons:///
+var pageLoadMessage = document.querySelector(".page-load-message"); //choose your reality 
+var pickYourGame = document.querySelector(".pick-your-game");//choose your fighter
+
+///icon containers:///
 var classicChoiceOptionsIcons = document.querySelector(".classic-choice-options-icons");
 var difficultChoiceOptions = document.querySelector(".difficult-choice-options");
-///change game button:///
-var changeTheGameButton = document.querySelector(".change-the-game-button");
 
+///icons:///
+var fighterIcons = document.querySelectorAll('.fighter-icons'); //all icons
+var choiceSection = document.getElementById('choice-section'); //icon container
+
+//counter//
+var humanScore = document.querySelector(".human-win-score"); //counter
+var aiScore = document.querySelector(".ai-win-score"); //counter
+
+
+///change game button:///
+var changeTheGameButton = document.querySelector(".change-the-game-button")
+
+
+var resultsIcons = document.querySelector(".results-icons"); //results icons container
 
 //Event Listeners:
 classicContainer.addEventListener('click', displayClassicGame)
 difficultContainer.addEventListener('click', displayDifficultGame)
+changeTheGameButton.addEventListener('click', changeGameHandler)
 
 
-//Global Variables:
 
 //Functions: 
 
-//creates player object:///
 function createPlayer(name, token) {
 var player = {
       name: name,
       token: token,
-      wins: 0,
+      wins: 0,   
+      fighter: 0
     }
     return player
   }
 
-///Brainstorming functions/// 👇 👇👇
-
-//define game type?? 
-  function createGame(gameType){
+  function createGame(gameType) {
     var game = {
-      player: createPlayer('Human', 'assets/neo.png'),
-      computer: createPlayer('AI', 'assets/mr.smith.png'),
-      type: gameType,
-      icons: gameType === 'classic' ? classicOptions : difficultOptions
-    }
-    return game
+      player: createPlayer('human', 'assets/neo.png'),
+      ai: createPlayer('ai', 'assets/agent-smith.png'),
+      gameType: gameType,
+      fighters: [] 
+    }    
+    currentGame = game
+    return currentGame
   }
 
-//updated winner score??
-function winnerScore() {
-  if (roundWinner === 'player') {
-    game.player.wins++;
-  } else if (roundWinner === 'computer') {
-    game.computer.wins++;
+  function getRandomIndex(array) {
+    return array[Math.floor(Math.random() * array.length)];
   }
-}
-//☝️☝️☝️☝️
+
+  function checkWin(playerChoice, aiChoice) {
+    if (playerChoice === aiChoice) {
+      return 'draw';
+    } else if (
+      (playerChoice === 'rock' && aiChoice === 'scissors') ||
+      (playerChoice === 'paper' && aiChoice === 'rock') ||
+      (playerChoice === 'scissors' && aiChoice === 'paper') ||
+      (playerChoice === 'the architect' && aiChoice === 'sentinel') ||
+      (playerChoice === 'sentinel' && aiChoice === 'the architect')
+    ) {
+      return 'player';
+    } else {
+      return 'ai';
+    }
+  }
 
 
 
@@ -82,6 +110,12 @@ function displayDifficultGame() {
   show(difficultChoiceOptions) 
 }
 
-
-
+function changeGameHandler(event) {
+  hide(difficultChoiceOptions)
+  hide(classicChoiceOptionsIcons)
+  show(classicContainer)
+  show(difficultContainer)
+  show(pickYourGame)
+  hide(changeTheGameButton)
+}
 
